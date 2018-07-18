2e0313bf34ab1a92a9c13a3d35b0f50b9b91971b
maxgalochkin <maksgalochkin@mail.ru>
2018-07-18 15:43:50 +0300

    add Readme.md

--- public/src/app-shell.js
+++ public/src/app-shell.js
@@ -9,16 +9,33 @@ class ShellApp extends PinchGesture(HTMLElement) { //[1]
     this.attachShadow({mode: "open"});
     window.joiStore = new JoiStore({
       Y: 40,
+      pickerX: 300,
+      pickerY: 300,
+      durationMs: 0,
+      pickerRotation: 1,
     });
     joiStore.observe([""], this.render.bind(this));
 
     setTimeout(() => joiStore.dispatch(Reducer.xChange), 10);
   }
 
-  render(state){
-    this.shadowRoot.innerHTML =`
-     <play-field></play-field>
-    `
-  }
+
+  render(state) {
+    this.style.transition = "all " + state.durationMs + "ms cubic-bezier(0.39, 0.58, 0.57, 1)";
+    this.shadowRoot.innerHTML = `
+<style>
+:host {
+  margin-top: ${state.pickerY}px;
+  margin-left: ${state.pickerX}px;
 }
+</style>
+            <link rel="stylesheet" type="text/css" href="../style/style.css">
+        <color-picker id="picker"></color-picker>
+        </div>
+    `;
+
+  }
+
+}
+
 customElements.define("shell-app", ShellApp);
--- public/src/state/Reducer.js
+++ public/src/state/Reducer.js
@@ -8,5 +8,17 @@ export class Reducer {
     return state
   }
 
+  static pickerCoord(state, detail) {
+    state = JoiGraph.setIn(state, "pickerX", state.pickerX + detail.distX);
+    state = JoiGraph.setIn(state, "pickerY", state.pickerY + detail.distY);
+    state = JoiGraph.setIn(state, "durationMs", detail.durationMs);
+    return state;
+  }
+
+
+
+static pickerRotation(state, detail){
+    state = JoiGraph.setIn(state, "pickerRotate", )
+}
 
 }
